106c97b8d44f35cecf2a6515c2287d6589b65fd4
Victor Babenko <victor@iterable.com>
2020-05-06 08:50:34 -0700

    Iterable SDK Integration (#1)
    * Iterable SDK integration - initialization and identity management
    - Add Gradle wrapper, change SDK dependency to 3.1.6
    - Change SDK dependency to 3.2.0, enable Jetifier
    - Add MPID support
    - Mirror placeholder email logic from S2S integration
    - Add support for customizing IterableConfig

---
+++ src/main/java/com/iterable/iterableapi/IterableConfigHelper.java
@@ -0,0 +1,19 @@
+package com.iterable.iterableapi;
+
+public class IterableConfigHelper {
+    public static IterableConfig.Builder createConfigBuilderFromIterableConfig(IterableConfig config) {
+        IterableConfig.Builder builder = new IterableConfig.Builder();
+        if (config != null) {
+            builder.setPushIntegrationName(config.pushIntegrationName);
+            builder.setUrlHandler(config.urlHandler);
+            builder.setCustomActionHandler(config.customActionHandler);
+            builder.setAutoPushRegistration(config.autoPushRegistration);
+            builder.setLegacyGCMSenderId(config.legacyGCMSenderId);
+            builder.setCheckForDeferredDeeplink(config.checkForDeferredDeeplink);
+            builder.setLogLevel(config.logLevel);
+            builder.setInAppHandler(config.inAppHandler);
+            builder.setInAppDisplayInterval(config.inAppDisplayInterval);
+        }
+        return builder;
+    }
+}
---
+++ src/main/java/com/mparticle/kits/IterableDeviceIdHelper.java
@@ -0,0 +1,38 @@
+package com.mparticle.kits;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import androidx.annotation.WorkerThread;
+
+import java.lang.reflect.Method;
+
+class IterableDeviceIdHelper {
+    @WorkerThread
+    static String getGoogleAdId(Context context) {
+        try {
+            Class AdvertisingIdClient = Class
+                    .forName("com.google.android.gms.ads.identifier.AdvertisingIdClient");
+            Method getAdvertisingInfo = AdvertisingIdClient.getMethod("getAdvertisingIdInfo",
+                    Context.class);
+            Object advertisingInfo = getAdvertisingInfo.invoke(null, context);
+            Method isLimitAdTrackingEnabled = advertisingInfo.getClass().getMethod(
+                    "isLimitAdTrackingEnabled");
+            Boolean limitAdTrackingEnabled = (Boolean) isLimitAdTrackingEnabled
+                    .invoke(advertisingInfo);
+            Method getId = advertisingInfo.getClass().getMethod("getId");
+            String advertisingId = (String) getId.invoke(advertisingInfo);
+            if (!limitAdTrackingEnabled) {
+                return advertisingId;
+            }
+        } catch (Exception ignored) {}
+        return null;
+    }
+
+    static String getAndroidID(Context context) {
+        try {
+            return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        } catch (Exception ignored) {}
+        return null;
+    }
+}
