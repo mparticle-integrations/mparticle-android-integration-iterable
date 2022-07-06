4c603cabc15399b1f8911cb6755f1ac89b12ad28
Felipe Coronado <f.coronado838@gmail.com>
2022-07-06 10:14:02 -0500

    Configured Kotlin in project and ported main and test classes.

---
+++ src/main/kotlin/com/iterable/iterableapi/IterableConfigHelper.kt
@@ -0,0 +1,20 @@
+package com.iterable.iterableapi
+
+object IterableConfigHelper {
+    fun createConfigBuilderFromIterableConfig(config: IterableConfig?): IterableConfig.Builder {
+        val builder = IterableConfig.Builder()
+        if (config != null) {
+            builder.apply {
+                setPushIntegrationName(config.pushIntegrationName)
+                setUrlHandler(config.urlHandler)
+                setCustomActionHandler(config.customActionHandler)
+                setAutoPushRegistration(config.autoPushRegistration)
+                setCheckForDeferredDeeplink(config.checkForDeferredDeeplink)
+                setLogLevel(config.logLevel)
+                setInAppHandler(config.inAppHandler)
+                setInAppDisplayInterval(config.inAppDisplayInterval)
+            }
+        }
+        return builder
+    }
+}
