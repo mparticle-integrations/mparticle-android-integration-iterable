c94c7ab09be031bd6103ddb6480d6453755e1c1e
Mansi-mParticle <159845845+Mansi-mParticle@users.noreply.github.com>
2026-01-22 13:56:13 -0500

    feat: Update Android Gradle plugin to 8.0.2 (#99)

--- src/main/kotlin/com/mparticle/kits/IterableDeviceIdHelper.kt
+++ src/main/kotlin/com/mparticle/kits/IterableDeviceIdHelper.kt
@@ -7,18 +7,22 @@ internal object IterableDeviceIdHelper {
     @WorkerThread
     fun getGoogleAdId(context: Context?): String? {
         try {
-            val advertisingIdClient = Class
-                .forName("com.google.android.gms.ads.identifier.AdvertisingIdClient")
-            val getAdvertisingInfo = advertisingIdClient.getMethod(
-                "getAdvertisingIdInfo",
-                Context::class.java
-            )
+            val advertisingIdClient =
+                Class
+                    .forName("com.google.android.gms.ads.identifier.AdvertisingIdClient")
+            val getAdvertisingInfo =
+                advertisingIdClient.getMethod(
+                    "getAdvertisingIdInfo",
+                    Context::class.java,
+                )
             val advertisingInfo = getAdvertisingInfo.invoke(null, context)
-            val isLimitAdTrackingEnabled = advertisingInfo.javaClass.getMethod(
-                "isLimitAdTrackingEnabled"
-            )
-            val limitAdTrackingEnabled = isLimitAdTrackingEnabled
-                .invoke(advertisingInfo) as Boolean
+            val isLimitAdTrackingEnabled =
+                advertisingInfo.javaClass.getMethod(
+                    "isLimitAdTrackingEnabled",
+                )
+            val limitAdTrackingEnabled =
+                isLimitAdTrackingEnabled
+                    .invoke(advertisingInfo) as Boolean
             val getId = advertisingInfo.javaClass.getMethod("getId")
             val advertisingId = getId.invoke(advertisingInfo) as String
             if (!limitAdTrackingEnabled) {
